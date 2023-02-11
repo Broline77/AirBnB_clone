64637d34b658742f26360b44158d1e647e7bd5ac
broline <brolineoronu3@gmail.com>
2023-02-10 20:24:31 -0800

    add exeption

--- console.py
+++ console.py
@@ -4,9 +4,23 @@
 import cmd
 import models
 from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-classes = {"BaseModel": BaseModel}
+classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+        }
 
 class HBNBCommand(cmd.Cmd):
     """ the command line for the hbnb """
@@ -37,7 +51,6 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         class_name = args[0]
-        print(args)
         if class_name not in classes:
             print("** class doesn't exist **")
             return
@@ -72,7 +85,7 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, args):
         """ deleting an instance"""
         args = args.split(" ")
-        if not args:
+        if len(args) < 1:
             print("** class name missing **")
             return
         class_name = args[0]
@@ -95,6 +108,113 @@ class HBNBCommand(cmd.Cmd):
             found = True
         if not found:
             print("** no instance found **")
+
+    def do_all(self, args):
+        """ function to print all the string representation of based no ornot based on the classes"""
+        args = args.split(" ")
+        class_name = args[0]
+        if class_name:
+            if class_name not in classes:
+                print("** class doesn't exist **")
+                return
+        else:
+            all_instances = models.storage.all()
+            for key, instances in all_instances.items():
+                print([str(instances)])
+        if class_name in classes:
+            all_instances = models.storage.all()
+            for key, instance in all_instances.items():
+                if class_name in key:
+                    print([str(instance)])
+
+    def do_update(self, args):
+        """ update an instance based on a class name an id"""
+        args = args.split(" ")
+        class_name = args[0]
+
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        if class_name not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        instance_id = args[1]
+        found = False
+        key = "{}.{}".format(class_name, instance_id)
+        all_instances = models.storage.all()
+        if len(args) < 3:
+            print("** atribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attribute_name = args[2]
+        attribute_value = args[3]
+
+        for key, instance in all_instances.items():
+            if instance_id in key:
+                found = True
+                if len(args) < 5:
+                    setattr(instance, attribute_name, attribute_value.strip('"'))
+                    models.storage.save()
+
+        if not found:
+            print("** no instance found **")
+            return
+    def precmd(self, line):
+        """Reformat command line for advanced command syntax.
+        Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
+        (Brackets denote optional fields in usage example.)
+        """
+        _cmd = _cls = _id = _args = ''  # initialize line elements
+
+        # scan for general formating - i.e '.', '(', ')'
+        if not ('.' in line and '(' in line and ')' in line):
+            return line
+
+        try:  # parse line left to right
+            pline = line[:]  # parsed line
+
+            # isolate <class name>
+            _cls = pline[:pline.find('.')]
+
+            # isolate and validate <command>
+            _cmd = pline[pline.find('.') + 1:pline.find('(')]
+            if _cmd not in HBNBCommand.dot_cmds:
+                raise Exception
+
+            # if parantheses contain arguments, parse them
+            pline = pline[pline.find('(') + 1:pline.find(')')]
+            if pline:
+                # partition args: (<id>, [<delim>], [<*args>])
+                pline = pline.partition(', ')  # pline convert to tuple
+
+                # isolate _id, stripping quotes
+                _id = pline[0].replace('\"', '')
+                # possible bug here:
+                # empty quotes register as empty _id when replaced
+
+                # if arguments exist beyond _id
+                pline = pline[2].strip()  # pline is now str
+                if pline:
+                    # check for *args or **kwargs
+                    if pline[0] is '{' and pline[-1] is'}'\
+                            and type(eval(pline)) is dict:
+                        _args = pline
+                    else:
+                        _args = pline.replace(',', '')
+                        # _args = _args.replace('\"', '')
+            line = ' '.join([_cmd, _cls, _id, _args])
+
+        except Exception as mess:
+            pass
+        finally:
+            return line
+
+        
 
 
 if __name__ == '__main__':
